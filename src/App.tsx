05db0ffc564d752a7084d89aa916db57cc8ed86d
orbeenshr <orbeenshr@gmail.com>
2022-08-22 17:51:49 +0545

    implemented: API that uses fetchQuizQuestions function and QuestionState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React, {useState} from "react";
+import {fetchQuizQuestions} from "./API";
 //components
 import QuestionCard from "./components/QuestionCard";
+//types
+import {QuestionState, Difficulty } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
 
   const [loading, setLoading] = useState(false);    //loading:read  setLoading:write
-  const [questions, setQuestions] = useState([]);   
+  const [questions, setQuestions] = useState<QuestionState[]>([]);   
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([])
-  const [score, setScore] = useState(0)
-  const [gameOver, setGameOver] = useState(true)
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () => {};
 
@@ -28,14 +39,14 @@ const App = () => {
       </button>
       <p className="scroe">Score:</p>
       <p>Loading Questions.....</p>
-      <QuestionCard                           //props 
+      {/* <QuestionCard                           //props of QuestionCard
         questionNr = {number + 1}
         totalQuestions = {TOTAL_QUESTIONS}  
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
@@ -44,3 +55,4 @@ const App = () => {
 };
 
 export default App;
+ 
