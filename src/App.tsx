aa80d2f94077355f497f10d6506523ba49945e4b
ArbinShrestha <orbeenshr@gmail.com>
2022-08-23 18:02:47 +0545

    implemnent start trivia

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,19 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+console.log(questions)
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
-  const startTrivia = async () => {};
+    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    
+    setQuestions(newQuestions);
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0);
+    setLoading(false);
+  };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
